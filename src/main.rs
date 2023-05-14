ab48dbcd6d1ad5289a9db42d3573926de81b258d
sidereior <alexanderlnanda@gmail.com>
2023-05-14 14:37:04 -0400

    main.rs updated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    //intitalize options
+    //declare vector string for filenames
+    //pbr init
+    //pbr cleanup
+    //return result with error loggging
+    
+    let mut options = Options::new();
+    let args: Args = Args::from_args();
+
+    //multithreading so that it preforms better
+    return 0;
 }
+
