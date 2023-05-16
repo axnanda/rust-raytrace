3f657ed4d4f360b5179c439da547441ad4f9f7d0
sidereior <alexanderlnanda@gmail.com>
2023-05-16 17:36:23 -0400

    fixed merges

--- src/main.rs
+++ src/main.rs
@@ -1,209 +1,53 @@
-/*
-fn main() {
-    use graplot::Plot3D;
-    //add abiclity to change the bounds of the graph
-    //you can't do 3 cos(&t&) because you have to explicetly identify the multiplicaitona dna division that takes place
-    use std::io::{stdin,stdout,Write};
-    let mut foft=String::new();
-    print!("Please enter the x component of some vector-valued function in the form (x(&t&))  ex: (3cos(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut foft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=foft.chars().next_back() {
-        foft.pop();
-    }
-    if let Some('\r')=foft.chars().next_back() {
-        foft.pop();
-    }
-    let mut goft=String::new();
-    print!("Please enter the y component of some vector-valued function in the form (y(&t&))  ex: (5sin(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut goft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=goft.chars().next_back() {
-        
-        goft.pop();
-    }
-    if let Some('\r')=goft.chars().next_back() {
-        goft.pop();
-    }
-    let mut hoft=String::new();
-    print!("Please enter the z component of some vector-valued function in the form (z(&t&))  ex: (2sin(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut hoft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=hoft.chars().next_back() {
-        hoft.pop();
-    }
-    if let Some('\r')=hoft.chars().next_back() {
-        hoft.pop();
-    }
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 
-    //set vales for xs
-    let mut xs = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
-    let mut ys = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
-    let mut zs = [0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.,0.];
-    //now need to add user input to specify the bounds of the graph
-    //and need to add ability to set bounds between
-    for n in 0..14 {
-        let mut m = n as i64;
-        m = m - 6;
-        let mut newfoft=foft.clone();
-        let mut newgoft=goft.clone();
-        let mut newhoft=hoft.clone();
-        newfoft = newfoft.replace("&t&", &m.to_string());
-        newgoft = newgoft.replace("&t&", &m.to_string());
-        newhoft = newhoft.replace("&t&", &m.to_string());
-        let myx = meval::eval_str(newfoft).unwrap();
-        let myy = meval::eval_str(newgoft).unwrap();
-        let myz = meval::eval_str(newhoft).unwrap();
-        xs[n]=myx as f64;
-        ys[n]=myy as f64;
-        zs[n]=myz as f64;
-        //why does cos or sin or tan not work
-    }
-    let plot = Plot3D::new((xs, ys, zs, "r-o"));
-    plot.show();
-}
-*/
+use image::{RgbImage, ImageBuffer, Rgb};
 
-use std::convert::TryInto;
-const SIZE: usize = 10;
-
-fn main() {
-    use graplot::Plot3D;
-    //add ability to change the bounds of the graph
-    //you can't do 3 cos(&t&) because you have to explicetly identify the multiplicaitona dna division that takes place
-    use std::io::{stdin,stdout,Write};
-    let mut foft=String::new();
-    print!("Please enter the x component of some vector-valued function in the form (x(&t&))  ex: (3cos(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut foft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=foft.chars().next_back() {
-        foft.pop();
-    }
-    if let Some('\r')=foft.chars().next_back() {
-        foft.pop();
-    }
-    let mut goft=String::new();
-    print!("Please enter the y component of some vector-valued function in the form (y(&t&))  ex: (5sin(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut goft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=goft.chars().next_back() {
-        
-        goft.pop();
-    }
-    if let Some('\r')=goft.chars().next_back() {
-        goft.pop();
-    }
-    let mut hoft=String::new();
-    print!("Please enter the z component of some vector-valued function in the form (z(&t&))  ex: (2sin(&t&)): ");
-    let _=stdout().flush();
-    stdin().read_line(&mut hoft).expect("You did not enter a correct vector-valued function, try again");
-    if let Some('\n')=hoft.chars().next_back() {
-        hoft.pop();
-    }
-    if let Some('\r')=hoft.chars().next_back() {
-        hoft.pop();
-    }
-    let mut stringEvals=String::new();
-    print!("How many points would you like to include in your graph:");
-    let _=stdout().flush();
-    //change this here!
-
-    stdin().read_line(&mut hoft).expect("You did not enter a correct vector-valued function, try again");//is this needed
-    if let Some('\n')=hoft.chars().next_back() {
-        hoft.pop();
-    }
-    if let Some('\r')=hoft.chars().next_back() {
-        hoft.pop();
-    }
-    //could you just choose a few traces such as all lines from y=x and rotate by some value and then use z values and draw circles
-
-    let numEvals = meval::eval_str(stringEvals).unwrap();
-    let numIter: i64 = numEvals as i64;
-    let numUsize: usize = numEvals as usize;
-    let mut xs = vec![0.; numUsize];
-    let mut ys = vec![0.; numUsize];
-    let mut zs = vec![0.; numUsize];
-    SIZE = numUsize;
-
-    //sidereior/rust-multivarcalc-grapher/src/main.rs
+fn main(){
+    const width: u32 = 500;
+    const height: u32 = 500;
     
+    //let mut buffer = String::from(format!("P3\n{} {}\n255\n", width, height));
+    //let reader = BufReader::new(file);
     
+    let mut buffer: RgbImage = ImageBuffer::new(width, height);
+    //make buffer of strings which everything will be put into
+    //tuple with for loop to iterate through each pixel
+    for (x, y, pixel) in buffer.enumerate_pixels_mut(){
+        let r = (255.999 * (x as f64 / (width-1) as f64)) as u8;
+        let g = (255.999 * (y as f64 / (height-1) as f64)) as u8;
+        let b = 0 as u8;
+        *pixel = Rgb([r, g, b]);
+        //IS THERE ERROR IN BUFFER???
+    }
     
-    //now need to add user input to specify the bounds of the graph
-    //and need to add ability to set bounds between
-    //lowest smoothing curve or just some way to generate splines between areas
-    for n in 0..numIter {
-        let mut m = n as i64;
-        m = m - numIter/2;
-        let mut newfoft=foft.clone();
-        let mut newgoft=goft.clone();
-        let mut newhoft=hoft.clone();
-        newfoft = newfoft.replace("&t&", &m.to_string());
-        newgoft = newgoft.replace("&t&", &m.to_string());
-        newhoft = newhoft.replace("&t&", &m.to_string());
-        let myx = meval::eval_str(newfoft).unwrap();
-        let myy = meval::eval_str(newgoft).unwrap();
-        let myz = meval::eval_str(newhoft).unwrap();
-        xs[n as usize]=myx as f64;
-        ys[n as usize]=myy as f64;
-        zs[n as usize]=myz as f64;
-        //why does cos or sin or tan not work
-    }
-    //then use the points to produce lowest smoothing curves between points
-    //WHAT IS TYPE ANNOTATIONS NEEDED?
+    buffer.save("image.png").unwrap();
+    /*
+    //write to file system
+    std::fs::write("rawout.ppm", buffer).expect("error");
+    let file = File::open("rawout.ppm").expect("error on file");
 
-    //instead just use vectors
+    let decoder = PNMDecoder::new(reader).expect("error on reader");
+    let image = decoder.read_image().expect("error on image");
+    let dynamic_image = DynamicImage::ImageRgb8(image.to_rgb8());
+    let path = Path::new("output.jpg");
+    let output = File::create(path).expect("error on output");
+    let mut encoder = JpegEncoder::new(output);
+    encoder
+        .encode_image(&dynamic_image)
+        .expect("Failed to encode image");
+
+     */
+    //intitalize options
+    //declare vector string for filenames
+    //pbr init
+    //pbr cleanup
+    //return result with error loggging
+    //start with converting ppm image to jpg
 
 
-    let mut fts = xs.to_array();
-    let mut gts = convert_vec_to_array(ys, SIZE);
-    let mut hts = convert_vec_to_array(zs, SIZE);
-    let plot = Plot3D::new((fts, gts, hts, "r-o"));
-    plot.show();
+    //multithreading so that it preforms better
+   
 }
 
-    
-
-fn convert_vec_to_array<T: Copy>(vec: Vec<T>, size: usize) -> [T; size] {
-    //instead of doing an array just do a vector
-    //copilot: write a declartion for a vector of size size
-
-
-    let mut array = [T::default(); size];
-    //must be a better way to do this
-    for (index, item) in vec.iter().enumerate() {
-        array[index] = *item;
-    }
-    array
-}
-
-macro_rules! convert_vec_to_array {
-    ($container:ident, $size:expr) => {{
-    if $container.len() != $size {
-            None
-    } else {
-        use std::mem;
-        let mut arr : [_; $size] = unsafe { mem::uninitialized() };
-        for element in $container.into_iter().enumerate() {
-            let old_val = mem::replace(&mut arr[element.0],element.1);
-            unsafe { mem::forget(old_val) };
-        }
-        Some(arr)
-        }
-    }};
-}
-
-
-fn vector_to_array<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
-}
-
-fn cast_vec<T, U>(vec: Vec<T>) -> Vec<U> {
-    unsafe {
-        let vec_ptr = vec.as_mut_ptr();
-        let vec_len = vec.len();
-        let vec_cap = vec.capacity();
-        Vec::from_raw_parts(vec_ptr as *mut U, vec_len, vec_cap)
-    }
-}
